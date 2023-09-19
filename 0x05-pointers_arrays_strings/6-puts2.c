b33b12070d70b8ac8208ff2e96710367dce7fa11
Zdtar <ofomahemmanuel@gmail.com>
2023-09-19 01:18:21 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,13 +12,13 @@ void puts2(char *str)
 	char *y = str;
 	int o;
 
-	while (*y != '\n')
+	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0 ; 0 <= t ; o++)
+	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 	{
